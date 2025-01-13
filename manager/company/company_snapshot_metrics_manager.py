6be05efa621dbaed1bf8d21a46926a8ed28ef7c6
George Philippas <george.philippas@outlook.com.gr>
2025-01-13 14:47:37 +0200

    Initial commit

--- manager/company/company_snapshot_metrics_manager.py
+++ manager/company/company_snapshot_metrics_manager.py
@@ -1,12 +1,12 @@
-from datetime import date
 from typing import Optional, Tuple
 
 from sqlmodel import Session, select
 
-from abstract.manager.manager import Manager, BaseModelBound, SQLModelBound
+from abstract.manager.manager import Manager
 from manager.company.company_manager import CompanyManager
 from model.company.company_snapshot_metrics import CompanySnapshotMetrics
 from schema.company.company import CompanySchema, CompanySnapshotMetricsSchema
+from sqlalchemy import and_
 
 
 class CompanySnapshotMetricsManager(Manager):
@@ -23,10 +23,14 @@ class CompanySnapshotMetricsManager(Manager):
 
         existing_company_ = self._company_manager.by_isin(company_schema_.isin)
 
-        query_ = select(CompanySnapshotMetrics).where(
-            CompanySnapshotMetrics.company_id == existing_company_.id and CompanySnapshotMetrics.updated_at == company_snapshot_metrics_schema_.date)
+        if existing_company_ is not None:
+            query_ = select(CompanySnapshotMetrics).where(
+                (and_(CompanySnapshotMetrics.company_id == existing_company_.id,
+                      CompanySnapshotMetrics.updated_at == company_snapshot_metrics_schema_.date)))
 
-        return self._session.exec(query_).first()
+            return self._session.exec(query_).first()
+        else:
+            return None
 
     def persist(self, schema_tuple: Tuple[CompanySchema, CompanySnapshotMetricsSchema],
                 foreign_keys: Optional[dict] = None) -> Optional[CompanySnapshotMetrics]:
