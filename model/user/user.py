6fc715e9c629ea2fa6d214d3fe23333120305319
George Philippas <george.philippas@outlook.com.gr>
2025-01-14 15:20:46 +0200

    Initial commit

---
+++ model/user/user.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = SQLModel.field(primary_key=True)
+
+    username: str = Field(nullable=False, unique=True)
+    password: str = Field(nullable=False)
+
+    email: str = Field(nullable=False, unique=True)
