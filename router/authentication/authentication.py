90091bfd4e67ed0b1ef0ba1b85796d9b8b61e228
George Philippas <george.philippas@outlook.com.gr>
2025-01-14 14:01:54 +0200

    Initial commit

--- router/authentication/authentication.py
+++ router/authentication/authentication.py
@@ -3,4 +3,6 @@ from router.router import authentication_router
 
 @authentication_router.get("/login")
 async def login():
-    return {"location": "/login"}
+    return {
+        "location": "/login"
+    }
