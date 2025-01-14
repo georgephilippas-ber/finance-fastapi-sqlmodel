194297378c8ac3fffa79b389b71b6e48f774a770
George Philippas <george.philippas@outlook.com.gr>
2025-01-14 15:17:47 +0200

    Initial commit

---
+++ core/jsonwebtoken/jsonwebtoken.py
@@ -0,0 +1,34 @@
+import jwt
+from jwt import ExpiredSignatureError, InvalidTokenError
+from typing import Dict, Optional
+from datetime import datetime, timedelta
+
+from configuration.encryption import JSON_WEB_TOKEN_SECRET_KEY, JSON_WEB_TOKEN_EXPIRATION_TIME_MINUTES
+
+
+class JSONWebToken:
+    _secret_key: str
+    _algorithm: str
+
+    def __init__(self, *, secret_key: Optional[str] = JSON_WEB_TOKEN_SECRET_KEY, algorithm: Optional[str] = 'HS256'):
+        self._secret_key = secret_key
+
+        self._algorithm = algorithm
+
+    def encode(self, payload: Dict, expiration_time_minutes: Optional[int] = JSON_WEB_TOKEN_EXPIRATION_TIME_MINUTES):
+        payload_ = {
+            **payload,
+            'exp': datetime.now() + timedelta(minutes=expiration_time_minutes)
+        }
+
+        jwt.encode(payload_, self._secret_key, algorithm=self._algorithm)
+
+        return payload_
+
+    def verify(self, token: str):
+        try:
+            return jwt.decode(token, self._secret_key, algorithms=[self._algorithm])
+        except (ExpiredSignatureError, InvalidTokenError) as e:
+            print(e)
+
+            return None
---
+++ instances/shared.py
@@ -0,0 +1,3 @@
+from core.jsonwebtoken.jsonwebtoken import JSONWebToken
+
+json_web_token = JSONWebToken()
