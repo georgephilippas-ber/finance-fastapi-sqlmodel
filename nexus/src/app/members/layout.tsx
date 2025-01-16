0aa68bbbbc02bedbb82812f41e7e38b8b3ad32d7
George Philippas <george.philippas@outlook.com.gr>
2025-01-16 15:00:32 +0200

    Initial commit

--- nexus/src/app/members/layout.tsx
+++ nexus/src/app/members/layout.tsx
@@ -6,10 +6,10 @@ export default function Layout({children}: { children: React.ReactNode; })
         <div className={"h-screen flex flex-col"}>
             <header>
                 <nav className="bg-white border-gray-200 dark:bg-gray-900 font-sans">
-                    <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+                    <div className="flex flex-wrap items-center justify-between mx-auto p-2">
                         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                             <img src={APPLICATION_NAVBAR_LOGO_URL} className="h-8" alt="logo"/>
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                                 Fluent Investor
                             </span>
                         </a>
@@ -22,11 +22,10 @@ export default function Layout({children}: { children: React.ReactNode; })
                                       d="M1 1h15M1 7h15M1 13h15"/>
                             </svg>
                         </button>
-                        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <div className="hidden w-full md:block md:w-auto mr-3">
                             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                 <li>
-                                    <a href="/"
-                                       className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500">
+                                    <a href="/" className="block font-sans text-white rounded  dark:text-white text-sm">
                                         Home
                                     </a>
                                 </li>
