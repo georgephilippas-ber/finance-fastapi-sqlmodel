5a7f3681dc1c1a38e847571a90bf7240bfb0c90f
George Philippas <george.philippas@outlook.com.gr>
2025-01-18 22:47:56 +0200

    Initial commit

--- configuration/ai.py
+++ configuration/ai.py
@@ -1,3 +1,3 @@
 AI_DEMO_ENABLED: bool = True
 
-AI_DEMO_SQL_QUERY: str = 'SELECT * FROM your_table_name ORDER BY RAND() LIMIT 10'
+AI_DEMO_SQL_QUERY: str = 'SELECT * FROM company ORDER BY RAND() LIMIT 10'
