a7942b0ec1bf9812b2f6941d86be9c54cb4b3e05
George Philippas <george.philippas@outlook.com.gr>
2025-01-19 08:46:14 +0200

    Initial commit

--- scripts/run-meilisearch-local.sh
+++ scripts/run-meilisearch-local.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-OS=$(uname)
-
+PORT=7700
 MASTER_KEY="jYRlnO2U1liwOSdm9CjTPFS1HZuBjeaLK3gh2NXcr28"
 
+OS=$(uname)
 if [[ "$OS" == "Linux" ]]; then
     :
 elif [[ "$OS" == "Darwin" ]]; then
     :
 elif [[ "$OS" == CYGWIN* || "$OS" == MINGW* || "$OS" == MSYS* ]]; then
-    meilisearch-windows-amd64 --master-key $MASTER_KEY
+    meilisearch-windows-amd64 --master-key $MASTER_KEY --http-addr "0.0.0.0:${PORT}"
 else
     echo "$OS"
 fi
