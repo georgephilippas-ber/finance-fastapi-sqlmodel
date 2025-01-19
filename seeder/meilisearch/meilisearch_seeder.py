7ec1728e971e53ed2302b9a5f9aede0ba4c13993
George Philippas <george.philippas@outlook.com.gr>
2025-01-19 10:45:52 +0200

    Initial commit

--- seeder/meilisearch/meilisearch_seeder.py
+++ seeder/meilisearch/meilisearch_seeder.py
@@ -1,3 +1,14 @@
+from core.meilisearch.meilisearch_client import MeilisearchClient
+from service.company.company_service import CompanyService
+
+
 class MeilisearchSeeder:
-    def __init__(self):
+    _meilisearch_client: MeilisearchClient
+    _company_service: CompanyService
+
+    def __init__(self, meilisearch_client: MeilisearchClient, company_service: CompanyService):
+        self._meilisearch_client = meilisearch_client
+        self._company_service = company_service
+
+    def seed(self):
         pass
