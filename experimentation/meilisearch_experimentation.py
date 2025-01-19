7d2169b34d3859d93d76b63077db0800c28671d1
George Philippas <george.philippas@outlook.com.gr>
2025-01-19 19:00:17 +0200

    Initial commit

--- experimentation/meilisearch_experimentation.py
+++ experimentation/meilisearch_experimentation.py
@@ -1,21 +1,6 @@
-import asyncio
-
-from core.search.meilisearch.meilisearch_client import MeilisearchClient
 from database.database import Database
-from service.company.company_overview_search_service import CompanyOverviewSearchService
-from service.company.company_service import CompanyService
 
 if __name__ == '__main__':
-    async def exec():
-        db = Database()
+    db = Database()
 
-        mclient_ = MeilisearchClient()
-        with db.create_session() as session:
-            c_service = CompanyService(session)
-
-        coss = CompanyOverviewSearchService(mclient_, c_service)
-
-        print(coss.company_overview_query("consumer"))
-
-
-    asyncio.run(exec())
+    print(db.get_create_tables_sql_query())
