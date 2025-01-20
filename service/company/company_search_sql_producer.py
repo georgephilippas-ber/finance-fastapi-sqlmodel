d2c232cfc6b47afc7ae48864d28a1649a52f438a
George Philippas <george.philippas@outlook.com.gr>
2025-01-20 12:09:52 +0200

    Initial commit

--- service/company/company_search_sql_producer.py
+++ service/company/company_search_sql_producer.py
@@ -1,3 +1,18 @@
+from enum import Enum
+from typing import Dict, Tuple
+
+from typing import TypeAlias
+
+
+class MetricType(Enum):
+    MARKET_CAPITALIZATION = 1
+
+
+MetricD: TypeAlias = Dict[MetricType, Tuple[str, str]]
+
+a: MetricD = {MetricType.MARKET_CAPITALIZATION: ("", "")}
+
+
 class CompanySearchSQLProducer:
     def __init__(self):
         pass
