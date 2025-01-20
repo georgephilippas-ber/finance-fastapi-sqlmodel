881388afd21596e24a6904c3b078f99b5f82c2fe
George Philippas <george.philippas@outlook.com.gr>
2025-01-20 09:48:43 +0200

    Initial commit

--- service/company/company_overview_search_service.py
+++ service/company/company_overview_search_service.py
@@ -1,24 +1,46 @@
-from typing import List
+from typing import List, Optional
+
+from sqlalchemy import Engine, text
+from sqlmodel import Session
 
 from core.search.meilisearch.meilisearch_client import MeilisearchClient
 from schema.company.company import CompanyOverviewSchema
-from service.company.company_service import CompanyService
 
 
 class CompanyOverviewSearchService:
-    _meilisearch_client: MeilisearchClient
-    _company_service: CompanyService
+    _meilisearch_client: Optional[MeilisearchClient]
+    _sql_engine: Optional[Engine]
 
     _index_name: str
 
-    def __init__(self, meilisearch_client: MeilisearchClient, company_service: CompanyService,
-                 index_name: str = "company"):
+    def __init__(self, *, sql_engine: Optional[Engine] = None, meilisearch_client: Optional[MeilisearchClient] = None,
+                 meilisearch_index_name: str = "company"):
+        self._sql_engine = sql_engine
         self._meilisearch_client = meilisearch_client
-        self._company_service = company_service
 
-        self._index_name = index_name
+        self._index_name = meilisearch_index_name
 
-    def company_overview_query(self, query: str) -> List[CompanyOverviewSchema]:
-        return list(
-            map(lambda document: CompanyOverviewSchema(**document),
-                self._meilisearch_client.search(self._index_name, query)))
+    def company_overview_meilisearch_query(self, query: str) -> Optional[List[CompanyOverviewSchema]]:
+        if self._meilisearch_client:
+            return list(
+                map(lambda document: CompanyOverviewSchema(**document),
+                    self._meilisearch_client.search(self._index_name, query)))
+        else:
+            print("!MeilisearchClient")
+
+            return None
+
+    def sql_query_by_company_id(self, company_id_list: List[int]):
+        if self._sql_engine:
+            with self._sql_engine.connect() as connection_:
+                connection_.execute(
+                    text("select * from company where id in :company_id_list", {"company_id_list": company_id_list}),
+                    {}).all()
+        else:
+            print("!Engine")
+
+            return None
+
+
+if __name__ == "__main__":
+    pass
