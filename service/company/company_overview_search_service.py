0f1549b6954be9c2e061d30a2e9a94f9ba98a1bc
George Philippas <george.philippas@outlook.com.gr>
2025-01-20 11:19:17 +0200

    Initial commit

--- service/company/company_overview_search_service.py
+++ service/company/company_overview_search_service.py
@@ -1,26 +1,30 @@
 from typing import List, Optional
 
 from sqlalchemy import Engine, text
-from sqlmodel import Session
 
 from core.search.meilisearch.meilisearch_client import MeilisearchClient
+from database.database import Database
 from schema.company.company import CompanyOverviewSchema
+from service.company.company_service import CompanyService
 
 
 class CompanyOverviewSearchService:
     _meilisearch_client: Optional[MeilisearchClient]
-    _sql_engine: Optional[Engine]
-
     _index_name: str
 
+    _company_service: CompanyService
+
     def __init__(self, *, sql_engine: Optional[Engine] = None, meilisearch_client: Optional[MeilisearchClient] = None,
+                 company_service: CompanyService,
                  meilisearch_index_name: str = "company"):
         self._sql_engine = sql_engine
         self._meilisearch_client = meilisearch_client
 
+        self._company_service = company_service
+
         self._index_name = meilisearch_index_name
 
-    def company_overview_meilisearch_query(self, query: str) -> Optional[List[CompanyOverviewSchema]]:
+    def meilisearch_query(self, query: str) -> Optional[List[CompanyOverviewSchema]]:
         if self._meilisearch_client:
             return list(
                 map(lambda document: CompanyOverviewSchema(**document),
@@ -30,17 +34,16 @@ class CompanyOverviewSearchService:
 
             return None
 
-    def sql_query_by_company_id(self, company_id_list: List[int]):
-        if self._sql_engine:
-            with self._sql_engine.connect() as connection_:
-                connection_.execute(
-                    text("select * from company where id in :company_id_list", {"company_id_list": company_id_list}),
-                    {}).all()
-        else:
-            print("!Engine")
+    def sql_query(self, company_id_list: List[int]) -> Optional[List[CompanyOverviewSchema]]:
 
-            return None
+        return self._company_service.company_overview(company_id_list)
 
 
 if __name__ == "__main__":
-    pass
+    db = Database()
+
+    with db.create_session() as session:
+        cs = CompanyService(session=session)
+        co = CompanyOverviewSearchService(sql_engine=db.get_engine(), company_service=cs)
+
+        print(co.sql_query([10, 20]))
