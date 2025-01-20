829b8cee34b9e8f0af8218b14f29c3c9c8bdb82a
George Philippas <george.philippas@outlook.com.gr>
2025-01-20 19:28:54 +0200

    Initial commit

--- service/company/company_search_sql.py
+++ service/company/company_search_sql.py
@@ -1,7 +1,5 @@
-from enum import Enum, auto
+from enum import Enum
 from typing import Dict, Tuple, List
-
-from typing import TypeAlias
 
 from model.GICS.GICS import GICSIndustry
 from model.continent.continent import Continent
