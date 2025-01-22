3ab0f6aeac062de1e6075acbbb22892aa45a508b
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 20:28:58 +0200

    Initial commit

--- nexus/src/app/members/page.tsx
+++ nexus/src/app/members/page.tsx
@@ -6,12 +6,12 @@ export default async function ()
     const query_result = await search("USD");
 
     return (
-        <div className={"w-full h-full pt-12 px-4"}>
+        <div className={"w-full flex flex-col h-full pt-12 px-4"}>
             <div className={"w-full flex flex-row gap-4"}>
                 <input className={"input w-full"} type={"text"} placeholder={"search"}/>
                 <button className={"btn btn-primary"}>Search</button>
             </div>
-            <div className={"w-full flex flex-col gap-4 my-10 mx-auto p-10"}>
+            <div className={"w-full flex-grow overflow-auto flex flex-col gap-4 my-10 mx-auto p-10"}>
                 {query_result.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
             </div>
         </div>
