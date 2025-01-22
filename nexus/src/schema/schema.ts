4da6c832048b3ddfc2a0258840c589efeed0f56c
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 12:27:45 +0200

    Initial commit

--- nexus/src/schema/schema.ts
+++ nexus/src/schema/schema.ts
@@ -1,15 +1,34 @@
-export type company_overview_type =
-    {
-        ticker_id: number;
-        company_id: number;
-        company_name: string;
-        ticker_code: string;
-        exchange_code: string;
-        currency_symbol: string;
-        gics_sector_name: string;
-        gics_industry_name: string;
-        company_logo_url: string;
-        country_flag_url: string;
-        currency_code: string;
-        description: string;
-    };
+type company_overview_type = {
+    company_id: number;
+    ticker_id: number;
+    company_name: string;
+    ticker_code: string;
+    exchange_code: string;
+    currency_symbol: string;
+    currency_code: string;
+    gics_sector_name: string;
+    gics_industry_name: string;
+    company_logo_url: string;
+    country_flag_url: string;
+    description: string;
+    country_common_name: string;
+    country_official_name: string;
+    country_cca2: string;
+    country_cca3: string;
+    continents: string;
+};
+
+type MetricDirectionType = 'DESC' | 'ASC';
+
+type MetricType =
+    | 'market_capitalization'
+    | 'return_on_assets'
+    | 'operating_profit_margin';
+
+type GroupType = 'GICSIndustry' | 'GICSSector' | 'country';
+
+type Criterion = {
+    metric: MetricType;
+    metric_direction: MetricDirectionType;
+    groups: Array<[GroupType | null, number]>;
+};
