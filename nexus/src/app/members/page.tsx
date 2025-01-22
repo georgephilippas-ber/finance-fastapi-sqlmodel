17d6ba0f6bdefaaaaca64061ed632d4ef0534e33
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 12:59:01 +0200

    Initial commit

--- nexus/src/app/members/page.tsx
+++ nexus/src/app/members/page.tsx
@@ -1,14 +1,16 @@
-import {getCompanyOverview} from "@/actions/financial/company";
 import {CompanyOverview} from "@/components/server/company/company-overview";
+import {search} from "@/actions/financial/company";
 
 export default async function ()
 {
-    const co = await getCompanyOverview([1, 2, 3]);
+    const query_result = await search("USD");
+
+    console.log(query_result);
 
     return (
         <div>
             Hi
-            {co.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
+            {query_result.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
         </div>
     );
 }
