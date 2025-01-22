779a886e662f4edcf5a173b1139f57858f342acb
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 14:45:54 +0200

    Initial commit

--- nexus/src/app/members/page.tsx
+++ nexus/src/app/members/page.tsx
@@ -5,12 +5,13 @@ export default async function ()
 {
     const query_result = await search("USD");
 
-    console.log(query_result);
-
     return (
-        <div>
-            Hi
-            {query_result.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
+        <div className={"w-full h-full"}>
+            <input className={"input w-full"} type={"text"} placeholder={"search"}/>
+asdas
+            <div className={"w-full flex flex-col gap-4 my-10 mx-auto p-10"}>
+                {query_result.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
+            </div>
         </div>
     );
 }
--- nexus/src/components/server/company/company-overview.tsx
+++ nexus/src/components/server/company/company-overview.tsx
@@ -3,11 +3,11 @@ import {company_overview_type} from "@/schema/schema";
 export function CompanyOverview({company_overview}: { company_overview: company_overview_type })
 {
     return (
-        <div className={"p-2"}>
-            <p>
-                Works!!!
-            </p>
-            {JSON.stringify(company_overview)}
+        <div className={"p-2 min-w-[10em] border border-gray-200 rounded-lg flex flex-row gap-2"}>
+            <div>
+
+            </div>
+            {/*{JSON.stringify(company_overview)}*/}
         </div>
     );
 }
