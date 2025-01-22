c29e7cdc7da34b11d54b46cb497423404926d686
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 14:15:41 +0200

    Initial commit

--- nexus/src/components/server/company/company-overview.tsx
+++ nexus/src/components/server/company/company-overview.tsx
@@ -3,7 +3,10 @@ import {company_overview_type} from "@/schema/schema";
 export function CompanyOverview({company_overview}: { company_overview: company_overview_type })
 {
     return (
-        <div>
+        <div className={"p-2"}>
+            <p>
+                Works!!!
+            </p>
             {JSON.stringify(company_overview)}
         </div>
     );
