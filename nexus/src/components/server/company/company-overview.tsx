12db272c0849add3c51618efb30fd05a730e079b
George Philippas <george.philippas@outlook.com.gr>
2025-01-22 20:13:38 +0200

    Initial commit

--- nexus/src/components/server/company/company-overview.tsx
+++ nexus/src/components/server/company/company-overview.tsx
@@ -4,8 +4,11 @@ export function CompanyOverview({company_overview}: { company_overview: company_
 {
     return (
         <div className={"p-2 min-w-[10em] border border-gray-200 rounded-lg flex flex-row gap-2"}>
-            <div>
-
+            <div className={"flex flex-col sm:hidden"}>
+                {company_overview.company_name}
+            </div>
+            <div className={"hidden sm:flex flex-col "}>
+                {company_overview.company_name} {company_overview.ticker_code}.{company_overview.exchange_code}
             </div>
             {/*{JSON.stringify(company_overview)}*/}
         </div>
