4f944e081310be0d8fec5dadd61c0b4f095eecb2
George Philippas <george.philippas@outlook.com.gr>
2025-01-23 09:32:53 +0200

    Initial commit

--- nexus/src/app/members/page.tsx
+++ nexus/src/app/members/page.tsx
@@ -7,6 +7,11 @@ export default async function ()
 {
     const query_result = await search("USD");
 
+    function handleClick(company_id: number)
+    {
+        console.log(company_id);
+    }
+
     return (
         <div className={"w-full flex flex-col h-full pt-12 px-4"}>
             <div className={"w-full flex flex-row gap-4"}>
@@ -14,7 +19,8 @@ export default async function ()
                 <button className={"btn btn-primary"}>Search</button>
             </div>
             <div className={"w-full flex-grow overflow-auto flex flex-col gap-4 my-10 mx-auto p-10"}>
-                {query_result.map((value, index) => <CompanyOverview key={index} company_overview={value}/>)}
+                {query_result.map((value, index) => <CompanyOverview onClick={handleClick} key={index}
+                                                                     company_overview={value}/>)}
             </div>
         </div>
     );
