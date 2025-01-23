5e6e8a500c3232e1742091f42dcbc273ec4bd0e1
George Philippas <george.philippas@outlook.com.gr>
2025-01-23 10:16:47 +0200

    Initial commit

--- nexus/src/app/members/layout.tsx
+++ nexus/src/app/members/layout.tsx
@@ -3,7 +3,7 @@ import {APPLICATION_NAVBAR_LOGO_URL} from "@/configuration/configuration";
 export default function Layout({children}: { children: React.ReactNode; })
 {
     return (
-        <div className={"h-screen flex flex-col"}>
+        <div className={"h-screen flex flex-col w-full"}>
             <header>
                 <nav className="bg-white border-gray-200 dark:bg-gray-900 font-sans">
                     <div className="flex flex-wrap items-center justify-between mx-auto p-2">
@@ -19,13 +19,9 @@ export default function Layout({children}: { children: React.ReactNode; })
             <main className="flex-grow overflow-y-auto">
                 {children}
             </main>
-            <footer className="h-10">
+            <footer className="h-8">
                 <div className="text-center text-gray-500 text-sm">
                     © {new Date().getFullYear()} <strong>Fluent Investor</strong>.
-                    <br/>
-                    Licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank"
-                                          rel="noopener noreferrer" className="text-blue-500 hover:underline">MIT
-                    License</a>.
                 </div>
             </footer>
         </div>
