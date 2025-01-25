3d86ddecc8b55a98ead61fe093bc9079dee87308
George Philippas <george.philippas@outlook.com.gr>
2025-01-25 10:39:03 +0200

    Initial commit

--- nexus/src/components/server/company/company-snapshot-metrics.tsx
+++ nexus/src/components/server/company/company-snapshot-metrics.tsx
@@ -10,6 +10,143 @@ function currencyFormat(value: number, currency_code: string): string
     }).format(value);
 }
 
+function percentageFormat(value: number): string
+{
+    return (value * 1.e2).toFixed(2) + "%";
+}
+
+function SizeMetrics({company_snapshot_metrics, currency_code}: {
+    company_snapshot_metrics: company_snapshot_metrics_type,
+    currency_code: string
+})
+{
+    return (
+        <div>
+            <p className={"text-2xl"}>
+                Company Size
+            </p>
+            <table className={"p-2 m-5"}>
+                <thead>
+                <tr>
+                    <th className={"w-[8em]"}>
+
+                    </th>
+                    <th className={"w-40"}>
+
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        MCAP
+                    </th>
+                    <td>
+                        {currencyFormat(company_snapshot_metrics.market_capitalization, currency_code)}
+                    </td>
+                </tr>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        EV
+                    </th>
+                    <td>
+                        {currencyFormat(company_snapshot_metrics.enterprise_value, currency_code)}
+                    </td>
+                </tr>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        ND
+                    </th>
+                    <td>
+                        {currencyFormat(company_snapshot_metrics.enterprise_value - company_snapshot_metrics.market_capitalization, currency_code)}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+function ProfitabilityMetrics({company_snapshot_metrics, currency_code}: {
+    company_snapshot_metrics: company_snapshot_metrics_type,
+    currency_code: string
+})
+{
+    return (
+        <div>
+            <p className={"text-2xl"}>
+                Profitability
+            </p>
+            <table className={"p-2 m-5"}>
+                <thead>
+                <tr>
+                    <th className={"w-[8em]"}>
+
+                    </th>
+                    <th className={"w-40"}>
+
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        OM
+                    </th>
+                    <td>
+                        {percentageFormat(company_snapshot_metrics.operating_profit_margin)}
+                    </td>
+                </tr>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        NPM
+                    </th>
+                    <td>
+                        {percentageFormat(company_snapshot_metrics.net_profit_margin)}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+function ReturnMetrics({company_snapshot_metrics, currency_code}: {
+    company_snapshot_metrics: company_snapshot_metrics_type,
+    currency_code: string
+})
+{
+    return (
+        <div>
+            <p className={"text-2xl"}>
+                Return
+            </p>
+            <table className={"p-2 m-5"}>
+                <thead>
+                <tr>
+                    <th className={"w-[8em]"}>
+
+                    </th>
+                    <th className={"w-40"}>
+
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th className={"text-left text-lg font-semibold"}>
+                        ROA
+                    </th>
+                    <td>
+                        {percentageFormat(company_snapshot_metrics.return_on_assets)}
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
 export function CompanySnapshotMetrics({company_snapshot_metrics, currency_code}: {
     company_snapshot_metrics: company_snapshot_metrics_type;
     currency_code: string;
@@ -17,49 +154,9 @@ export function CompanySnapshotMetrics({company_snapshot_metrics, currency_code}
 {
     return (
         <div className={"font-sans w-4/5 m-2"}>
-            <div>
-                <p className={"text-2xl"}>
-                    Company Size
-                </p>
-                <table className={"p-2 m-5"}>
-                    <thead>
-                    <tr>
-                        <th className={"w-[8em]"}>
-
-                        </th>
-                        <th className={"w-40"}>
-
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th className={"text-left text-lg font-semibold"}>
-                            MCAP
-                        </th>
-                        <td>
-                            {currencyFormat(company_snapshot_metrics.market_capitalization, currency_code)}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className={"text-left text-lg font-semibold"}>
-                            EV
-                        </th>
-                        <td>
-                            {currencyFormat(company_snapshot_metrics.enterprise_value, currency_code)}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th className={"text-left text-lg font-semibold"}>
-                            ND
-                        </th>
-                        <td>
-                            {currencyFormat(company_snapshot_metrics.enterprise_value - company_snapshot_metrics.market_capitalization, currency_code)}
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+            <SizeMetrics company_snapshot_metrics={company_snapshot_metrics} currency_code={currency_code}/>
+            <ProfitabilityMetrics company_snapshot_metrics={company_snapshot_metrics} currency_code={currency_code}/>
+            <ReturnMetrics company_snapshot_metrics={company_snapshot_metrics} currency_code={currency_code}/>
         </div>
     );
 }
