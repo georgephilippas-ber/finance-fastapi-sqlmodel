98fd3c6dc1ef1458a21c33c82f3cfe3460ca7bb9
George Philippas <george.philippas@outlook.com.gr>
2025-01-25 09:23:46 +0200

    Initial commit

--- nexus/src/components/server/company/company-overview.tsx
+++ nexus/src/components/server/company/company-overview.tsx
@@ -6,6 +6,19 @@ export default function CompanyOverview({company_overview}: { company_overview: 
 {
     return (
         <div className={"font-sans w-full m-1"}>
+            <div className={"flex flex-row items-center justify-start gap-4"}>
+                <div className={"m-2"}>
+                    <span className={"text-2xl"}>
+                        {company_overview.ticker_code}
+                    </span>.
+                    <span className={"font-semibold"}>
+                        {company_overview.exchange_code}
+                    </span>
+                </div>
+                <div className={"font-semibold text-lg"}>
+                    {company_overview.currency_code}
+                </div>
+            </div>
             <details open>
                 <summary className={"text-lg"}>Description</summary>
                 <p className={"text-sm text-justify p-4"}>
