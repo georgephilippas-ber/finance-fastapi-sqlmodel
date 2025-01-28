c68509ac4272c982f5690d4615bb9cb8264864bc
George Philippas <george.philippas@outlook.com.gr>
2025-01-28 10:09:30 +0200

    Initial commit

--- nexus/src/configuration/configuration.ts
+++ nexus/src/configuration/configuration.ts
@@ -18,8 +18,17 @@ const NEXUS_PRODUCTION_SERVER_BASE_URL = 'http://localhost:3000'
 
 export const NEXUS_SERVER_BASE_URL = ENVIRONMENT == EnvironmentType.DEVELOPMENT ? NEXUS_DEVELOPMENT_SERVER_BASE_URL : NEXUS_PRODUCTION_SERVER_BASE_URL;
 
+// APPLICATION
+
+export const HAS_LANDING_PAGE: boolean = false;
+
+export const APPLICATION_PROTECTED_HOME_URL = `${NEXUS_SERVER_BASE_URL}/members/company/search`;
+export const APPLICATION_HOME_URL = `${NEXUS_SERVER_BASE_URL}`;
+export const APPLICATION_NAVBAR_LOGO_URL = "https://flowbite.com/docs/images/logo.svg";
+
 
 // SECURITY
+
 const SECURITY_ENABLED: boolean = true;
 
 export const JSON_WEB_TOKEN_SECRET_KEY: string = 'bb07c2d34569be2cdf090daf5ae58daec3c998265d8685820288d620c3b279d2'
@@ -29,13 +38,6 @@ export const JSON_WEB_TOKEN_SECRET_KEY_ENCODED = new TextEncoder().encode(JSON_W
 export const PROTECTED_ROUTES = SECURITY_ENABLED ? [
     '/members'
 ] : [];
-
-
-// APPLICATION
-
-export const APPLICATION_PROTECTED_HOME_URL = `${NEXUS_SERVER_BASE_URL}/members/company/search`;
-export const APPLICATION_HOME_URL = `${NEXUS_SERVER_BASE_URL}`;
-export const APPLICATION_NAVBAR_LOGO_URL = "https://flowbite.com/docs/images/logo.svg";
 
 
 // INTERNATIONALIZATION
