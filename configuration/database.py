ce8024dae77a0aef6e2965d59bca3ee1f7101eec
George Philippas <george.philippas@outlook.com.gr>
2025-01-29 20:21:50 +0200

    Initial commit

--- configuration/database.py
+++ configuration/database.py
@@ -7,21 +7,18 @@ from core.utilities.root import project_root
 
 
 class DBMSType(Enum):
-    DUCKDB_FILE = auto()
-    DUCKDB_MEMORY = auto()
+    DUCKDB = auto()
     MYSQL = auto()
     POSTGRESQL = auto()
 
 
-DBMS: DBMSType = DBMSType.DUCKDB_FILE
+DBMS: DBMSType = DBMSType.DUCKDB
 
 if DBMS == DBMSType.MYSQL:
     DATABASE_URL: str = f"mysql://root:development@localhost:3306/{PROJECT_NAME.replace('-', '_')}"
 elif DBMS == DBMSType.POSTGRESQL:
     DATABASE_URL: str = f"postgresql+psycopg2://root:development@localhost:5432/${PROJECT_NAME.replace('-', '_')}"
-elif DBMS == DBMSType.DUCKDB_FILE:
+elif DBMS == DBMSType.DUCKDB:
     DATABASE_URL: str = f"duckdb:///{join(project_root(), "database", "files", f"{PROJECT_NAME}-{ENVIRONMENT.value}-duckdb.db")}"
-elif DBMS == DBMSType.DUCKDB_MEMORY:
-    DATABASE_URL: str = f"duckdb:///:memory:"
 
 SESSION_DATABASE_FILENAME: str = join(project_root(), "database", "files", "session.db")
