42ad8e9383ccb667cc7fed5ee73c8fcad048b5a5
George Philippas <george.philippas@outlook.com.gr>
2025-02-02 14:56:30 +0200

    Initial commit

--- experimentation/highcharts-experimentation/src/App.tsx
+++ experimentation/highcharts-experimentation/src/App.tsx
@@ -33,88 +33,164 @@ import './App.css';
 // }
 
 import Highcharts from 'highcharts';
+import {DateTime, Duration} from "luxon";
+import {useEffect} from "react";
+
+import {faker} from "@faker-js/faker";
 
 
+type entry_type =
+    {
+        date: string;
+        value: number;
+    }
 
-(async () =>
+type chart_data_type =
+    {
+        title: string;
+        subtitle?: string;
+        series_name: string;
+        dependent_axis_title: string;
+        data: entry_type[];
+    }
+
+
+export function SingleSeriesChart({chart_data, tooltip_point_format = '{series.name}: <b>{point.y:.2f}%</b>'}: {
+    chart_data: chart_data_type;
+    tooltip_point_format?: string;
+})
 {
+    const element_id = crypto.randomUUID().replace(/-/g, '');
 
-    const data = await fetch(
-        'usdeur.json'
-    ).then(response => response.json());
-
-    Highcharts.chart('container', {
-        chart: {
-            zooming: {
-                type: 'x'
-            }
-        },
-        title: {
-            text: 'USD to EUR exchange rate over time'
-        },
-        subtitle: {
-            text: document.ontouchstart === undefined ?
-                'Click and drag in the plot area to zoom in' :
-                'Pinch the chart to zoom in'
-        },
-        xAxis: {
-            type: 'datetime'
-        },
-        yAxis: {
-            title: {
-                text: 'Exchange rate'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        plotOptions: {
-            area: {
-                marker: {
-                    radius: 2
-                },
-                lineWidth: 1,
-                color: {
-                    linearGradient: {
-                        x1: 0,
-                        y1: 0,
-                        x2: 0,
-                        y2: 1
+    useEffect(() =>
+    {
+        Highcharts.chart(element_id,
+            {
+                chart: {
+                    zooming: {
+                        type: 'x'
                     },
-                    stops: [
-                        [0, 'rgb(199, 113, 243)'],
-                        [0.7, 'rgb(76, 175, 254)']
-                    ]
+                    backgroundColor: 'rgba(100, 100, 100, 0.0)',
                 },
-                states: {
-                    hover: {
-                        lineWidth: 1
+                title: {
+                    text: chart_data.title,
+                    style:
+                        {
+                            color: '#fff',
+                        }
+                },
+                subtitle: {
+                    text: chart_data.subtitle,
+                    style:
+                        {
+                            color: '#fff',
+                        }
+                },
+                xAxis: {
+                    type: 'datetime',
+                    tickInterval: 365 * 24 * 3600 * 1000,
+                    labels:
+                        {
+                            style:
+                                {
+                                    fontSize: '0.75em',
+                                    color: '#fff'
+                                },
+                            formatter: function ()
+                            {
+                                return DateTime.fromMillis(this.value as number).toFormat('MM/yyyy');
+                            }
+                        }
+                },
+                yAxis: {
+                    gridLineWidth: 0,
+                    title: {
+                        text: chart_data.dependent_axis_title,
+                        style:
+                            {
+                                fontWeight: '600',
+                                fontSize: "1.05em",
+                                color: '#fff'
+                            }
+                    },
+                    labels:
+                        {
+                            style:
+                                {
+                                    color: '#fff'
+                                }
+                        }
+
+                },
+                legend: {
+                    enabled: false
+                },
+                tooltip:
+                    {
+                        pointFormat: tooltip_point_format,
+                    },
+                plotOptions: {
+                    area: {
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        color: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, 'rgb(130, 120, 190)'],
+                                [0.7, 'rgb(90, 100, 160)']
+                            ]
+                        },
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null
                     }
                 },
-                threshold: null
-            }
-        },
+                series: [{
+                    type: 'area',
+                    name: chart_data.series_name,
+                    data: chart_data.data.map(value => [DateTime.fromISO(value.date).toMillis(), value.value]),
+                }]
+            });
+    }, [])
 
-        series: [{
-            type: 'area',
-            name: 'USD to EUR',
-            data: data
-        }]
-    });
-})();
+    return (
+        <div style={{borderRadius: "0.75em", width: "100%"}} id={element_id}>
+            <p>Loading...</p>
+        </div>
+    );
+}
 
 
 function App()
 {
     return (
-        <figure className="highcharts-figure">
-            <div id="container"></div>
-            <p className="highcharts-description">
-                Highcharts has extensive support for time series, and will adapt
-                intelligently to the input data. Click and drag in the chart to zoom in
-                and inspect the data.
-            </p>
-        </figure>
+        <div style={{margin: "0 auto", maxWidth: "80svw"}}>
+            <SingleSeriesChart chart_data={
+                {
+                    title: "Return on Investment",
+                    subtitle: "Net Income / Assets",
+                    dependent_axis_title: "return on investment: %",
+                    series_name: "ROI",
+                    data: Array(100).fill(0).map((_value, index) =>
+                    {
+                        return {
+                            date: DateTime.now().plus(Duration.fromObject({years: index})).toISODate(),
+                            value: faker.number.float({min: 23, max: 100})
+                        }
+                    })
+                }
+            }/>
+        </div>
     );
 }
 
