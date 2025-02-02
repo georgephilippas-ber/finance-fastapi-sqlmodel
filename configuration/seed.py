b11338bd1ae0e94c417b44a5501be3d5a04ec8c9
George Philippas <george.philippas@outlook.com.gr>
2025-02-02 09:37:21 +0200

    Initial commit

--- configuration/seed.py
+++ configuration/seed.py
@@ -8,8 +8,8 @@ from configuration.project import PROJECT_NAME
 from core.environment.environment import EnvironmentType, is_running_in_docker
 from schema.user.user import UserSchema
 
-SEED_ON_STARTUP: bool = False
-DROP_ALL_TABLES_BEFORE_SEEDING: bool = False
+SEED_ON_STARTUP: bool = True
+DROP_ALL_TABLES_BEFORE_SEEDING: bool = True
 
 if is_running_in_docker():
     SEED_ON_STARTUP: bool = False
